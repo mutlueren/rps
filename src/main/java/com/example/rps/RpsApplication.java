104fbdbfb9938a1fa7b6c32310d51460fc598e43
Mutlu Eren <mutlueren01@gmail.com>
2022-04-30 20:07:01 +0300

    cors

--- src/main/java/com/example/rps/RpsApplication.java
+++ src/main/java/com/example/rps/RpsApplication.java
@@ -2,6 +2,10 @@ package com.example.rps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RpsApplication {
@@ -9,5 +13,13 @@ public class RpsApplication {
     public static void main(String[] args) {
         SpringApplication.run(RpsApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/rps").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
--- src/main/java/com/example/rps/controller/RpsController.java
+++ src/main/java/com/example/rps/controller/RpsController.java
@@ -4,6 +4,7 @@ import com.example.rps.service.RpsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class RpsController {
     RpsService rpsService;
 
     @RequestMapping("/rps")
+    @CrossOrigin(origins = "http://localhost:8080")
     public ResponseEntity rpsRequest(@RequestParam("choise") String choise) throws Exception {
         return ResponseEntity.status(HttpStatus.OK).body(rpsService.rpsBrain(choise));
     }
